e5fed432a16e1c64cbadb64ae84daa3683573806
tyobi <maeda.ryo.529@gmail.com>
2021-03-13 21:51:20 +0900

    first commit

---
+++ src/Blog.jsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import Article from './Article';
+// import { Foo, Bar } from './components/FooBar';
+import * as FooBar from './components/FooBar';
+import Hoge from './components/Hoge'
+
+const Blog = () => {
+  // componentDidMount() 
+  // {
+  //   //ボタンがクリックされたらいいねをカウントアップする    
+  //   document.getElementById('counter').addEventListener('click', this.countUp)
+  // }
+
+  // componentDidUpdate()
+  // {    
+  //   if (this.state.count >= 10)
+  //   {
+  //     this.setState({count: 0})  
+  //   }
+  // }
+
+  // componentWillUnmount() {
+  //   document.getElementById('counter').removeEventListener('click', this.countUp)
+  // }
+
+
+  // countUp = () => {
+  //   this.setState({count: this.state.count + 1})
+  // }
+
+
+  return (
+    <>
+      <Article
+        title={"Reactの使い方"}
+        // count={this.state.count}
+      />
+      <FooBar.Foo />
+      <FooBar.Bar />
+      <Hoge />
+    </>
+    
+  )
+  
+} 
+
+export default Blog
